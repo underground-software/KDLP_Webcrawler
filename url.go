67f3bd80f84d3e8f1a166f789c2685d930bca723
ccowman <cowmanconor@gmail.com>
2023-07-25 22:00:32 +0100

    Added functions for checking URL is valid/internal

---
+++ url.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/url"
+	"strings"
+)
+
+// Checks whether a given URL is valid
+func isValidURL(URL string) bool {
+	_, err := url.ParseRequestURI(URL)
+	return err == nil
+}
+
+// Checks whether a URL is internal
+func isInternalURL(URL string) bool {
+
+	return strings.HasPrefix(URL, domain)
+
+}
