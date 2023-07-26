d36a063dee5cdc0b7a93d29f3c169119ed8ab506
ccowman <cowmanconor@gmail.com>
2023-07-26 17:21:28 +0100

    Added test deadLinksToFile

--- crawl_test.go
+++ crawl_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"os"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -79,6 +81,66 @@ func Test_newCrawler(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := newCrawler(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("newCrawler() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCrawler_writeDeadLinksToFile(t *testing.T) {
+	type fields struct {
+		deadLinks []string
+	}
+	type args struct {
+		filepath string
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		args       args
+		wantErr    bool
+		wantOutput []string // Expected output lines in the file
+	}{
+		{
+			name: "Write dead links to file",
+			fields: fields{
+				deadLinks: []string{"https://www.example.com/deadlink1", "https://www.example.com/deadlink2"},
+			},
+			args: args{
+				filepath: "test_dead_links.txt",
+			},
+			wantErr:    false,
+			wantOutput: []string{"https://www.example.com/deadlink1", "https://www.example.com/deadlink2"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Crawler{
+				deadLinks: tt.fields.deadLinks,
+			}
+			defer os.Remove(tt.args.filepath) // Cleanup the temporary file
+
+			err := c.writeDeadLinksToFile(tt.args.filepath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Crawler.writeDeadLinksToFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !tt.wantErr {
+				// Read the file and check its content
+				fileContent, err := os.ReadFile(tt.args.filepath)
+				if err != nil {
+					t.Errorf("Failed to read the file: %v", err)
+					return
+				}
+
+				outputLines := strings.Split(string(fileContent), "\n")
+				// Ignore the last empty line
+				outputLines = outputLines[:len(outputLines)-1]
+
+				// Check if the output matches the expected output
+				if !reflect.DeepEqual(outputLines, tt.wantOutput) {
+					t.Errorf("Unexpected file content. Got: %v, want: %v", outputLines, tt.wantOutput)
+				}
 			}
 		})
 	}
