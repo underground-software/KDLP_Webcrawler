11b4cd3d3f97006bf604d11d7e77f8c272dde5aa
ccowman <cowmanconor@gmail.com>
2023-07-26 13:38:51 +0100

    Added func to retrieve HTTP content + Refactored

--- url.go
+++ url.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -17,17 +19,54 @@ func isInternalURL(URL string) bool {
 	return strings.HasPrefix(URL, domain)
 }
 
-// Function to fetch URL's HTTP status code
-func checkURLStatus(URL string) (int, error) {
-
-	// Fetch HTTP contents of URL
+// Function to fetch HTTP response
+func fetchHTTPResponse(URL string) (*http.Response, error) {
 	resp, err := http.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// Function to read HTTP response body
+func readHTTPResponseBody(resp *http.Response) (string, error) {
+	content, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+// Function to check URL's HTTP status code
+func checkURLStatus(URL string) (int, error) {
+	resp, err := fetchHTTPResponse(URL)
 	if err != nil {
 		return 0, err
 	}
-	defer resp.Body.Close()
-
-	// Return status code
 	return resp.StatusCode, nil
+}
 
+// Function to retrieve the HTTP content of a URL page
+func retrieveHTTPContent(URL string) (string, error) {
+	statusCode, err := checkURLStatus(URL)
+	if err != nil {
+		return "", err
+	}
+
+	if statusCode < 200 || statusCode >= 300 {
+		return "", fmt.Errorf("failed to fetch content: received status code %d", statusCode)
+	}
+
+	resp, err := fetchHTTPResponse(URL)
+	if err != nil {
+		return "", err
+	}
+
+	content, err := readHTTPResponseBody(resp)
+	if err != nil {
+		return "", err
+	}
+
+	return content, nil
 }
