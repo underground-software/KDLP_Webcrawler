5b08f7b071727041e51e0615ecf941e70b533388
ccowman <cowmanconor@gmail.com>
2023-07-27 13:53:40 +0100

    Moved constants to Crawler struct

--- constants.go
+++
@@ -1,6 +0,0 @@
-package main
-
-const (
-	domain  = "https://kdlp.underground.software/"
-	homeURL = domain + "index.html"
-)
--- structs.go
+++ structs.go
@@ -2,6 +2,12 @@ package main
 
 type Crawler struct {
 
+	// Stores base domain of website being crawled
+	domain string
+
+	// Starting URL for crawler
+	homeURL string
+
 	// Map to track visited URLs
 	visited map[string]bool
 
